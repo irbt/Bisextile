440f0e46aaf5e78ada9d7757b43c8fdeed24b0ba
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2022-11-26 19:51:24 +0100

    version finale

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Program
+{
+
+    class Bisextile
+    {
+
+        static public void Main()
+        {
+
+            static void IfYearIsLeap(int year)
+            {
+
+                if ((year % 4 == 0) && (year % 100 != 0))
+                {
+                    Console.WriteLine(year + " est une année bisextile");
+                }
+                else
+                {
+                    Console.WriteLine(year + " n'est PAS une année bisextile");
+                }
+
+            }
+
+
+            IfYearIsLeap(2016);
+            IfYearIsLeap(2018);
+            IfYearIsLeap(2012);
+
+
+        }
+    }
+}
